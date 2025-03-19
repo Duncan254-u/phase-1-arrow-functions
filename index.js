e12a9be20631ecefa15ec0c34585fc9d6ea7df10
Duncan Kibet <kibet2704@gmail.com>
2025-03-18 21:57:38 -0700

    update changes made

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide (num1,num2) {
+return num1/num2;
+}
+
+const square = (num) => num*num;
+    console.log (square);
+
+    
+const add = (num1, num2) => num1 + num2;
+add(num1,num2);
